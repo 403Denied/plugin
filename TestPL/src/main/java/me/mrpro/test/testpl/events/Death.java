0b592a48f6411624d8b8b316d48b1ff9baca0b90
nonbinarynerd <66492638+nonbinarynerd@users.noreply.github.com>
2022-06-07 18:53:55 -0500

    Update Death.java
    skull drop

--- TestPL/src/main/java/me/mrpro/test/testpl/events/Death.java
+++ TestPL/src/main/java/me/mrpro/test/testpl/events/Death.java
@@ -1,18 +1,31 @@
 package me.mrpro.test.testpl.events;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
 
 public class Death implements Listener {
-
-
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
-        event.setKeepInventory(true);
-        event.setKeepLevel(true);
-        event.setDeathMessage(ChatColor.GRAY + "[" + ChatColor.DARK_RED + "☠" + ChatColor.GRAY + "] " + ChatColor.GRAY + "Oof!");
+        Player player = event.getEntity();
+        Player pkiller = player.getKiller();
+        ItemStack skull = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta meta = (SkullMeta) skull.getItemMeta();
+        meta.setOwningPlayer(event.getEntity());
+        skull.setItemMeta(meta);
+        if(pkiller instanceof Player) {
+            event.setDeathMessage(ChatColor.GRAY + "[" + ChatColor.DARK_RED + "☠" + ChatColor.GRAY + "] " + ChatColor.GRAY + "Oof! " + ChatColor.RED + player.getDisplayName() + ChatColor.GRAY + " Was Slain By " + ChatColor.RED + pkiller.getDisplayName());
+            player.getLocation().getWorld().dropItemNaturally(player.getLocation(), skull);
+        } else {
+            event.setDeathMessage(ChatColor.GRAY + "[" + ChatColor.DARK_RED + "☠" + ChatColor.GRAY + "] " + ChatColor.GRAY + "Oof! " + ChatColor.RED + player.getDisplayName() + ChatColor.GRAY + " Died!");
+        }
+
     }
-}
+
+};
